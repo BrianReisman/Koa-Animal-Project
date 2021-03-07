e8e75956d692d406e5aa73ae26b1df8ce3854662
BrianReisman <brianreisman@protonmail.com>
2021-03-06 23:26:53 -0500

    using koa and koa-router making async request to 2 apis. 2 endpoints set up and rendering to the screen

--- index.js
+++ index.js
@@ -1,39 +1,123 @@
-import Koa from 'koa';
-import Router from '@koa/router';
-import cors from '@koa/cors';
+import Koa from "koa";
+import Router from "@koa/router";
+import cors from "@koa/cors";
 
 // Commented out so it doesn't get auto-removed by text editors. You may uncomment if needed.
-// import fetch from 'isomorphic-fetch';
+import fetch from "isomorphic-fetch";
+
+//*my code
+// fetch("https://catfact.ninja/fact")
+// .then((res) => res.json())
+// .then((res) => {
+// console.log(res.fact);
+// quote.textContent = res.fact
+// })
+// .catch((err) => console.log(err));
+//*my code
 
 // Used for reading incoming POST bodies. Commented out for same reason as above.
 // See https://github.com/dlau/koa-body#usage-with-koa-router
-// import koaBody from 'koa-body';
+// import koaBody from "koa-body";
 
 const app = new Koa();
 const router = new Router();
 const port = 3011;
 
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: "*" }));
 
-router.get('/', (ctx) => {
-	ctx.body = 'hello!';
+router.get("/", (ctx) => {
+  ctx.body = "hello!";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//* acpet any get request to the given path
+
+router.get("/dogPix", async (ctx) => {
+  const response =  await fetch(`https://dog.ceo/api/breed/african/images/random`) //!need dynamic value for god.ceo call
+	ctx.body = await response.json()
+});
+
+
+//?try/catch
+//?callback
+//?async/await
+
+
+router.get("/catFact", async (ctx) => {
+  const response =  await fetch(`https://catfact.ninja/fact`)
+	ctx.body = await response.json()
+});
+
+//*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get("X-Response-Time");
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 });
 
 app.use(async (ctx, next) => {
-	await next();
-	const rt = ctx.response.get('X-Response-Time');
-	console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-});
-
-app.use(async (ctx, next) => {
-	const start = Date.now();
-	await next();
-	const ms = Date.now() - start;
-	ctx.set('X-Response-Time', `${ms}ms`);
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", `${ms}ms`);
 });
 
 app.use(router.routes());
 
 app.listen(port, () => {
-	console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
--- web/main.js
+++ web/main.js
@@ -3,24 +3,15 @@ const quote = document.querySelector(".quote");
 const input = document.querySelector(".form-control");
 const form = document.querySelector("form");
 
-form.addEventListener("submit", (e) => {  
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  
-  fetch("https://catfact.ninja/fact")
-  .then((res) => res.json())
-  .then((res) => {
-    // console.log(res.fact);
-    quote.textContent = res.fact
 
-  })
-  .catch((err) => console.log(err));
-  
-  fetch(`https://dog.ceo/api/breed/${input.value}/images/random`)
-  .then((res) => res.json())
-  .then((res) => {
-    // console.log(res.message);
-    image.setAttribute('src', res.message)
-  })
-  .catch((err) => console.log(err));
-  
+  fetch("http://localhost:3011/dogPix")
+    .then((res) => res.json())
+    .then((res) => image.setAttribute("src", res.message))
+    .catch((err) => console.log(err));
+  fetch("http://localhost:3011/catFact")
+    .then((res) => res.json())
+    .then((res) => (quote.textContent = res.fact))
+    .catch((err) => console.log(err));
 });
